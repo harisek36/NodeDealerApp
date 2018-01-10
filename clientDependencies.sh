0aea6fee6f49e86e8ab75bd4a3c77503c0a36261
harishsekar <harisek36@gmail.com>
2018-01-10 04:02:18 -0600

    Feature(Client+Server): Added Script files for dependencies installation

---
+++ clientDependencies.sh
@@ -0,0 +1,4 @@
+cd Client-Module
+sudo npm --save-dev install @angular/cli
+sudo npm install ng-common
+ng serve &  #webpack compilation
---
+++ serverDependencies.sh
@@ -0,0 +1,6 @@
+# node_module installation : server side
+
+npm install
+node -v
+npm -v
+node app.js
